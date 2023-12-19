9c2383ab824860aaec8cfbe7586dc93150213a3c
MatheusBigui <153760019+MatheusBigui@users.noreply.github.com>
2023-12-19 18:20:56 -0300

    v2(background)

--- relógio/script.js
+++ relógio/script.js
@@ -2,6 +2,7 @@ const segundos = document.getElementById('segundos');
 const minutos = document.getElementById('minutos');
 const horas = document.getElementById('horas');
 var currentTime = new Date().getHours();
+const fundo = document.getElementById('relogio');
 
 const relogio = setInterval(function time() {
     let dateToday = new Date();
@@ -26,3 +27,27 @@ const relogio = setInterval(function time() {
     segundos.textContent = sec;
 })
 
+
+function setFundoCor() {
+    const currentTime = new Date().getHours();
+
+    if (currentTime >= 6 && currentTime < 12) {
+        fundo.style.backgroundImage = "url('images/manha.jpg')";
+    } else if (currentTime >= 12 && currentTime < 18) {
+        fundo.style.backgroundImage = "url('images/tarde.jpg')";
+    } else {
+        fundo.style.backgroundImage = "url('images/noite.jpg')";
+    }
+}
+
+setFundoCor();
+relogio;
+
+function adjustRelogioSize() {
+    var relogio = document.getElementById('relogio');
+} if (window.matchMedia("(max-width: 700px)").matches) {
+    relogio.style.fontSize = '3em';
+} else {
+    relogio.style.fontSize = '5em';
+}
+window.addEventListener('resize', adjustRelogioSize);
